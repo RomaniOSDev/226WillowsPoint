7a73d92e1dd5fee1e7cd33956b72102f111dc944
Roman <romazaniosdev@gmail.com>
2026-06-17 15:29:30 +0300

    Initial Commit

---
+++ 226WillowsPoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  226WillowsPoint
+//
+//  Created by Roman on 6/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
